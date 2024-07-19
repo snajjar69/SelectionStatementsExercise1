40a50b636f3f6d8444860389675d93d4eca70c21
snajjar69 <snajjar69@gmail.com>
2024-07-19 11:14:14 -0400

    Added Exercise New

--- SelectionStamentsExcersise-NEW/Program.cs
+++ SelectionStamentsExcersise-NEW/Program.cs
@@ -1,9 +1,28 @@
-﻿namespace SelectionStamentsExcersise_NEW;
+﻿using System.Runtime.InteropServices.JavaScript;
+
+namespace SelectionStamentsExcersise_NEW;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        var r = new Random();
+        var favNum = r.Next(1, 10);
+        Console.WriteLine(favNum);
+        
+        var userInput = int.Parse(Console.ReadLine());
+        
+        if (userInput < favNum)
+        {
+            Console.WriteLine($"{userInput} is too low");
+        }
+        else if (userInput > favNum)
+        {
+            Console.WriteLine($"{userInput} is too high");
+        }
+        else
+        {
+            Console.WriteLine($"you guessed it");
+        }
     }
 }
